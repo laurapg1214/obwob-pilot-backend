15d6496af3743f374d5845558c8dbc7b9e13e5cf
Laura Purcell-Gates <laurapg1214@users.noreply.github.com>
2025-01-07 15:22:02 +0000

    add v1 routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,28 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
-  # Can be used by load balancers and uptime monitors to verify that the app is live.
-  get "up" => "rails/health#show", as: :rails_health_check
-
-  # Render dynamic PWA files from app/views/pwa/*
-  get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
-  get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
-
-  # Defines the root path route ("/")
-  # root "posts#index"
+  mount RailsAdmin::Engine => 'admin', as: 'rails_admin'
+  
+  # SET APP ROOT
+  root 'events#questions' 
+  
+  # EVENTS - v2
+  # get 'events'
+  # get 'events/:id'
+  # get 'events/:id/questions'
+  
+  # QUESTIONS - v3
+  # get 'questions'
+  # post 'questions'
+  # put 'questions/:id/mark_active'
+  # put 'questions/:id/mark_inactive'
+  # put 'questions/:id/mark_assigned'
+  # put 'questions/:id/mark_unassigned'
+  # put 'questions/:id/mark_complete'
+  # put 'questions/:id/mark_incomplete'
+  
+  # SESSIONS
+  post 'sessions' => 'sessions#create'
+  delete 'sessions' => 'sessions#destroy'
+  
+  # REDIRECT ALL ROUTES TO ROOT
+  get '*path' => redirect('/')
 end
