9de7ee80babc6246df9d8729d6429fa595856b89
Laura Purcell-Gates <laurapg1214@users.noreply.github.com>
2025-01-09 18:25:10 +0000

    created eventquestions_controller, added update function

---
+++ app/controllers/eventquestions_controller.rb
@@ -0,0 +1,12 @@
+class Api::EventQuestionsController < ApplicationController
+  ### v3 ###
+  # # update questions
+  # def update
+  #   question = Question.find(params[:id])
+  #   if question.update(status: params[:status])
+  #     render json: { message: "Question updated successfully"}, status: :ok
+  #   else
+  #     render json: { errors: question.errors.full_messages }, status: :unprocessable_entity
+  #   end
+  # end
+end
