5ad26f7e7acfd0a06f879346b8b2da0091e88904
Laura Purcell-Gates <laurapg1214@users.noreply.github.com>
2025-01-09 18:24:40 +0000

    updated v2 & v3 routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,27 +1,37 @@
 Rails.application.routes.draw do
   mount RailsAdmin::Engine => 'admin', as: 'rails_admin'
   
-  # SET APP ROOT
+  # set app root
   root 'events#questions' 
-  
-  # EVENTS - v2
-  # get 'events'
-  # get 'events/:id'
-  # get 'events/:id/questions'
-  
-  # QUESTIONS - v3
-  # get 'questions'
-  # post 'questions'
-  # put 'questions/:id/mark_active'
-  # put 'questions/:id/mark_inactive'
-  # put 'questions/:id/mark_assigned'
-  # put 'questions/:id/mark_unassigned'
-  # put 'questions/:id/mark_complete'
-  # put 'questions/:id/mark_incomplete'
-  
-  # SESSIONS
-  post 'sessions' => 'sessions#create'
-  delete 'sessions' => 'sessions#destroy'
+
+  # nesting all CRUD routes under namespace (adds /api in front of all routes)
+  namespace :api do
+    # EVENTS 
+    ### v2 ###
+    # get 'events' => 'events#index'
+    # get 'events/:id' => 'events#index_by_current_event'
+
+    # QUESTIONS 
+    ### v3 ###
+    # get 'questions' => 'questions#index' # all questions across events
+    # post 'questions' => 'questions#create'
+    
+    # EVENT QUESTIONS 
+    ### v2 ###
+    # get 'events/:id/questions' => 'eventquestions#questions'
+
+    ### v3 ###
+    # single resource for updating questions (active/inactive, assigned/unassigned, complete/incomplete)
+    # resources :events do
+      # resources :questions, only: [:index, :update]
+      ## :index → GET /questions → Maps to index, used to list all questions
+      ## :update → PUT /questions/:id → Maps to update, used to update a specific question
+    # end
+    
+    # SESSIONS
+    post 'sessions' => 'sessions#create'
+    delete 'sessions' => 'sessions#destroy'
+  end 
   
   # REDIRECT ALL ROUTES TO ROOT
   get '*path' => redirect('/')
