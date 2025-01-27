5fe7c72feab6ee5b2e42492815f934a9e6637729
Laura Purcell-Gates <laurapg1214@users.noreply.github.com>
2025-01-27 12:21:28 +0000

    add emoji question type

--- app/controllers/api/responses_controller.rb
+++ app/controllers/api/responses_controller.rb
@@ -17,19 +17,35 @@ class Api::ResponsesController < ApplicationController
       return
     end
 
-    response = question.responses.new(response_params)
-    unless response.save
-      render json: { message: "Failed to create response.", errors: response.errors.full_messages }, status: :unprocessable_entity
+    # validation: only emoji type questions can have numeric response_value
+    if question.emoji? && !Response::EMOJI_VALUES.keys.include?(response_params[:response_value].to_i)
+      render json: { message: "Invalid emoji response." }, status: unprocessable_entity
       return
     end
 
-    render json: { message: "Response created successfully.", response: response }, status: :created 
+    response = question.responses.new(response_params)
+    unless response.save
+      render json: { 
+        message: "Failed to create response.", 
+        errors: response.errors.full_messages 
+      }, status: :unprocessable_entity
+      return
+    end
+
+    render json: { 
+      message: "Response created successfully.", 
+      response: response 
+      }, status: :created 
   end
 
   private
 
   def response_params
-    params.require(:response).permit(:participant_id, :response_text)
+    params.require(:response).permit(
+      :participant_id, 
+      :response_text, 
+      :response_value
+    )
   end
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_01_24_141930) do
+ActiveRecord::Schema[8.0].define(version: 2025_01_27_115557) do
   create_table "events", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
@@ -40,6 +40,7 @@ ActiveRecord::Schema[8.0].define(version: 2025_01_24_141930) do
     t.integer "question_type", default: 0
     t.string "content"
     t.boolean "default_record", default: false
+    t.string "emoji_type"
   end
 
   create_table "responses", force: :cascade do |t|
